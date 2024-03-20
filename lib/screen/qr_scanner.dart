b8ba03030f299645efd7aaaf13d4fd14236b2061
unknown <rojanparajuli13@gmail.com>
2024-03-20 12:26:52 +0545

    fix

--- lib/screen/qr_scanner.dart
+++ lib/screen/qr_scanner.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
-import 'package:http/http.dart' as http;
-import 'dart:io';
 import 'package:url_launcher/url_launcher.dart';
 
 class QrScanner extends StatefulWidget {
